38658e8bd5ea1cdeaf3906d85f9c39c43c92118c
George Chen <gchen@zoiclabs.com>
2024-01-19 23:52:08 -0800

    test husky again

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  let a = 'b'
+  let a = 'c'
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
